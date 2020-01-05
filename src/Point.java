5b8551e2d983d3e116446bfdebea04d25cf259c3
Yaroslavtsev, Alexey <A.Yaroslavtsev@x5.ru>
2020-01-06 00:51:29 +0300

    Point class

---
+++ src/Point.java
@@ -0,0 +1,21 @@
+public final class Point {
+    private final double x;
+    private final double y;
+
+    Point(double x, double y){
+        this.x=x;
+        this.y=y;
+    }
+    Point(){
+        this.x=0;
+        this.y=0;
+    }
+
+    public double getX(){
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+}
