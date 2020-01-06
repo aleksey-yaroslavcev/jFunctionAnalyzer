f7c619e5a4d90806f66af203758afb0533531dd7
Yaroslavtsev, Alexey <A.Yaroslavtsev@x5.ru>
2020-01-07 00:39:24 +0300

    say no to magic numbers in PolynomFunctions

--- src/ru/pp/yarik/FunctionAnalyzer/PolynomFunction.java
+++ src/ru/pp/yarik/FunctionAnalyzer/PolynomFunction.java
@@ -2,21 +2,33 @@ package ru.pp.yarik.FunctionAnalyzer;
 
 public class PolynomFunction extends Function {
 
+    static final String POLYNOM_DESCRIPTION_PREFIX="Polinomial function of order ";
+    static final String COEFF_PREFIX="a";
+    static final double DEFAULT_COEFF=1;
+
     PolynomFunction(int order){
         this.order=order;
 
         for (int i=0;i<=order;i++){
-            setParam("a"+i,1);
+            setCoeff(i,DEFAULT_COEFF);
         }
 
-        description="Polinomial function of order " + order;
+        setDescription(POLYNOM_DESCRIPTION_PREFIX + order);
     }
+
+    public void setCoeff(int index, double coeff){
+        setParam(COEFF_PREFIX+index,coeff);
+    }
+    public double getCoeff(int index) throws Exception {
+        return getParam(COEFF_PREFIX+index);
+    }
+
     @Override
     public double value(double x) throws Exception {
         double res=0;
         double a;
         for(int i=0;i<=order;i++){
-            res+=getParam("a"+i)*x;
+            res+=getCoeff(i)*x;
         }
         return res;
     }
